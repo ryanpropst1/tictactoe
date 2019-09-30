9e64cd597f321b372ee39f8adc4d1d28b5eeaf32
Ryan Propst <ryanpropst@gmail.com>
2019-09-30 16:04:57 -0500

    added start to package.json files

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 let express = require("express");
 let path = require("path");
 let app = express();
+const PORT = process.env.PORT || 3500
 
 app.use("/", express.static(path.join(__dirname, "../client")));
 
@@ -10,4 +11,8 @@ app.get("/", function(req, res) {
   res.sendFile(_dirname, "index.html");
 });
 
-app.listen(3000, "127.0.0.1");
+//app.listen(3000, "127.0.0.1");
+
+app.listen(PORT, function() {
+   console.log("You are now listening to port " + PORT +".");
+});
